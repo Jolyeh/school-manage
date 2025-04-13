e8513de409ddec8d1510aa1110f1c6b9e5bba18d
jolyeh <yehezekocolojoelprince@gmail.com>
2025-04-13 04:31:05 +0100

    ajout de lien

--- resources/views/include/model.blade.php
+++ resources/views/include/model.blade.php
@@ -25,9 +25,11 @@
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="{{ asset('template/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- Template Stylesheet -->
     <link href="{{ asset('template/css/style.css') }}" rel="stylesheet">
+
 </head>
 
 <body>
